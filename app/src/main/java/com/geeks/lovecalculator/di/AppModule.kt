cf7c1d9bad4ed5b25b3a677b72dd9bb07f2169d7
RedNeksi <126886432+RedNeksi7@users.noreply.github.com>
2023-07-30 23:18:51 +0600

    Add files via upload

---
+++ app/src/main/java/com/geeks/lovecalculator/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.geeks.lovecalculator.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.geeks.lovecalculator.data.Pref
+import com.geeks.lovecalculator.remote.LoveApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+
+    @Provides
+    fun provideApi(): LoveApi {
+        return Retrofit.Builder().baseUrl("https://love-calculator.p.rapidapi.com/")
+            .addConverterFactory(GsonConverterFactory.create()).build().create(LoveApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providePref(@ApplicationContext context: Context): Pref {
+        return Pref(context)
+    }
+}
---
+++ app/src/main/java/com/geeks/lovecalculator/ui/ResultFragment.kt
@@ -0,0 +1,57 @@
+package com.geeks.lovecalculator.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.geeks.lovecalculator.databinding.FragmentResultBinding
+import com.geeks.lovecalculator.model.LoveModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ResultFragment : Fragment() {
+
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+    private var loveModel: LoveModel? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loveModel = arguments?.getSerializable(LoveCalculatorFragment.LOVEMODEL_KEY) as LoveModel
+
+        initTextView()
+        initClickers()
+    }
+
+    private fun initClickers() {
+        with(binding) {
+            btnTryAgain.setOnClickListener {
+                findNavController().navigateUp()
+            }
+        }
+    }
+
+    private fun initTextView() {
+        with(binding) {
+            tvFistName.text = loveModel?.firstName.toString()
+            tvSecondName.text = loveModel?.secondName.toString()
+            tvPercentage.text = loveModel?.percentage.toString() + "%"
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
